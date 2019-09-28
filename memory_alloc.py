48c593809f19ffd7a2b9a6d3259e6f730c90cd9d
arshita.gupta <arshitagupta27@gmail.com>
2019-09-28 17:21:07 -0400

    updated files

--- memory_alloc.py
+++ memory_alloc.py
@@ -22,7 +22,6 @@ class MemoryAllocation():
     ):
         self.model = model
         self.input_type = input.type()
-        self.batch_size = input.shape[0]
 
     def get_num_params(self, ) -> int:
         """
@@ -30,6 +29,7 @@ class MemoryAllocation():
         """
         total_params = 0
         for param_tensor in self.model.state_dict():
+            # print(param_tensor, "\t", self.model.state_dict()[param_tensor].size())
             params_layer = 1
             for each_dim in self.model.state_dict()[param_tensor].size():
                 params_layer = params_layer * each_dim
@@ -38,13 +38,13 @@ class MemoryAllocation():
 
     def get_memory_params(self, total_params, ) -> int:
         """
-        Multiplies the total nnumber of parameters in the model with the batch_size and type of input.
+        Multiplies the total nnumber of parameters in the model with the type of input.
         The 'input_type' indicates whether the input is 4, 8, 16, 32 bits.
         """
         if self.input_type == "torch.FloatTensor":
-            return total_params*32*self.batch_size
+            return total_params*32
         elif self.input_type == "torch.DoubleTensor":
-            return total_params*64*self.batch_size
+            return total_params*64
         else:
             return Exception("Cannot understand input type.")
 
@@ -55,17 +55,17 @@ class MemoryAllocation():
         total_params = self.get_num_params()
         memory_needed = self.get_memory_params(total_params)
         print("Total params in the given model: ", total_params)
-        print("Total memory required: ", memory_needed)
+        print("Total memory required (including model parameters, input_type and batch_size):", memory_needed, "bits.")
 
         if torch.cuda.is_available():
             nvmlInit()
             handle = nvmlDeviceGetHandleByIndex(0)
             info = nvmlDeviceGetMemoryInfo(handle)
             total_device_memory = info.total
-            print("GPU Memory available: ", info.total)
+            print("GPU Memory available: ", info.total, "bytes")
         else:
             mem = virtual_memory()
             total_device_memory = mem.total
-            print("CPU Memory available: ", total_device_memory)
-        used = (memory_needed/total_device_memory)*100
-        print("Percentage of memory used after loading the given model: ", used)
+            print("CPU Memory available: ", total_device_memory, "bytes")
+        used = ((memory_needed/8)/total_device_memory)*100
+        print("Percentage of memory used after loading the given model:", '%.2f' % used, "%")
